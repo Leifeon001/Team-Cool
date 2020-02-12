667acc9fa4c7456d645800d65657fe581c3159ab
Leifeon001 <tristen.howk@yahoo.com>
2020-02-12 16:17:45 -0500

    adding newest work
    Adding fixes to character and the levels

---
+++ inferno/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    CharacterController Char;
+    public float speed;
+    Vector3 MoveDir;
+    public float gravity = 10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Char = GetComponent<CharacterController>();        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveDir = transform.TransformDirection(transform.forward);
+        MoveDir.y -= gravity * Time.deltaTime;
+        Char.Move((MoveDir * speed) * Time.deltaTime);
+    }
+}
