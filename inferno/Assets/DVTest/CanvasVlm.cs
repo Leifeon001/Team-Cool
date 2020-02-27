314586d4ccd29a15cfd01b46dd8ef8b33494b871
torchandgas <7064360@gmail.com>
2020-02-27 01:16:33 -0500

    Canvas changes
    Added change option for graphic quality and resolution. Added click sound for level 1. Adjusted Canvas.

--- inferno/Assets/DVTest/CanvasVlm.cs
+++ inferno/Assets/DVTest/CanvasVlm.cs
@@ -2,28 +2,67 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class CanvasVlm : MonoBehaviour
 {
     [SerializeField] float volunmeNum;
     [SerializeField] AudioMixer audioMixerV;
+    [SerializeField] Dropdown resolutionDropdown;
+    Resolution[] resolutionsVar;
 
-    // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        ResolustionControl();
     }
 
     public void VolunmeControl(float volume)
     {
-        //Debug.Log("VolunmeTest"); 
-        //Debug.Log(volume);
         audioMixerV.SetFloat("AMVolumeP",volume);
     }
+
+    public void QualityControl (int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void ResolustionControl()
+    {
+        resolutionsVar = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < resolutionsVar.Length; i++)
+        {
+            string option = resolutionsVar[i].width + "x" + resolutionsVar[i].height;
+            options.Add(option);
+
+            if (resolutionsVar[i].width == Screen.currentResolution.width && 
+                resolutionsVar[i].height ==Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutionsVar[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, 0);
+    }
+
+    //public void FullScreenControl (bool isFullScreen)
+    //{
+    //    Screen.fullScreen = isFullScreen;
+    //}
+
+    //void Update()
+    //{
+
+    //}
 }
